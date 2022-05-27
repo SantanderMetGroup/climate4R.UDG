aba0f5baaa6f48151e5c40448a602cbec8854ed3
zequihg50 <ezequiel.cimadevilla@unican.es>
2022-05-27 12:59:31 +0200

    using post instead of get

--- R/loginUDG.R
+++ R/loginUDG.R
@@ -23,18 +23,15 @@ loginUDG <- function(username, password, proxy.host = NULL, proxy.port = NULL) {
       }
       username.enc <- URLencode(username, reserved = TRUE)
       password.enc <- URLencode(password, reserved = TRUE)
-      url.check <- paste0("https://data.meteo.unican.es/udg-tap/rest/v1/signin/verify?username=", username.enc,
-                          "&password=", password.enc)
       message("[",Sys.time(), "] Setting credentials...")
-      con <- tryCatch(getURL(url.check, ssl.verifypeer = FALSE), error = function(er) {
-            er <- NULL
-            return(er)
+      res <- tryCatch(POST("https://data.meteo.unican.es/udg-tap/rest/v1/signin/verify/secure", body = list(username=username, password=password), encode = "form"), error = function(err) {
+          err <- NULL
+          return(err)
       })
-      if (is.null(con)) {
+      if (is.null(res)) {
             stop("Could not establish a connection.", call. = FALSE)
       } else {
-            b <- readLines(textConnection(con))
-            if (grepl("SUCCESS", strsplit(b, split = "\\\""))) {
+            if(content(res, as="parsed")$response == "SUCCESS") {
                   message("[",Sys.time(), "] Success!\nGo to <https://data.meteo.unican.es/udg-tap/home> for details on your authorized groups and datasets")
             } else {
                   stop("User name and password do not match\nPlease check your registration details or visit <https://data.meteo.unican.es/udg-tap/home> if in doubt")
@@ -46,7 +43,3 @@ loginUDG <- function(username, password, proxy.host = NULL, proxy.port = NULL) {
       J("ucar.httpservices.HTTPFactory")$setCredentials("data.meteo.unican.es", username, password)
 }
 # End
-
-
-
-
